ceb3a39f4b393bf14e9b0ba828675cb4d44e07bd
YatoNorai <edilsonmauro1727@gmail.com>
2026-04-06 17:02:59 -0300

    up

--- lib/src/theme/expressive_theme_data.dart
+++ lib/src/theme/expressive_theme_data.dart
@@ -267,7 +267,7 @@ extension ExpressiveThemeVariantX on ExpressiveThemeVariant {
           shape: roundedMedium,
           padding: EdgeInsets.symmetric(
             horizontal: tokens.spacing24,
-            vertical: tokens.spacing4,
+            vertical: tokens.spacing1,
           ),
         ),
       ),
